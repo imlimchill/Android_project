f4bdcf3598f062d29df287de28489547fb2abf3c
imlimchill <cojwd300@gmail.com>
2019-11-18 23:24:24 +0900

    임채정

--- lockScreen/app/src/main/java/com/cookandroid/lockscreen/LockActivity.java
+++ lockScreen/app/src/main/java/com/cookandroid/lockscreen/LockActivity.java
@@ -4,27 +4,41 @@ import android.app.Activity;
 import android.app.KeyguardManager;
 import android.app.KeyguardManager.KeyguardLock;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
+
+import com.ncorti.slidetoact.SlideToActView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 public class LockActivity extends Activity {
 
-//    Context context;
+    static String[] words = new String[200];
+    static String[] meaning = new String[200];
 
-    /** 퀴즈에서 사용될  */
-    TextView tvWord;
+    GregorianCalendar today = new GregorianCalendar ( );
+    int day = today.get ( today.DAY_OF_MONTH );
+    int pos = day * 10 - 11;
 
-    /** 잠금화면에서 사용될 각 퀴즈의 답과 오답을 저장할 버튼을 선언 */
-    Button btnAnswer1;
-    Button btnAnswer2;
-    Button btnAnswer3;
-    Button btnAnswer4;
-
+    SlideToActView slide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,25 +53,65 @@ public class LockActivity extends Activity {
 
         setContentView(R.layout.lock);
 
-        /** 단어와 답, 오답의 초기화 */
-        tvWord = (TextView)findViewById(R.id.tvWord);
-        btnAnswer1 = (Button)findViewById(R.id.btnAnswer1);
-        btnAnswer2 = (Button)findViewById(R.id.btnAnswer2);
-        btnAnswer3 = (Button)findViewById(R.id.btnAnswer3);
-        btnAnswer4 = (Button)findViewById(R.id.btnAnswer4);
+        readWordData();
 
-//        btnAnswer1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//               // if(btnAnswer1.getText().equals(??)) {
-//                    Toast.makeText(getApplicationContext(), "정답입니다.", Toast.LENGTH_SHORT).show();
-//                    // TODO 종료
-////                } else {
-////                    Toast.makeText(getApplicationContext(), "오답입니다. 다시 시도해보세요.", Toast.LENGTH_SHORT).show();
-////                }
-//            }
-//        });
+        if(day == 0) {
+            pos = 0;
+        }
+
+        ListView list = (ListView)findViewById(R.id.listLock);
+
+        final ArrayList<String> Items = new ArrayList<String>(); // 빈 데이터 리스트 생성
+
+        for (int idx = 1; idx < 11; idx++) {
+            Items.add(words[pos] + "    " + meaning[pos]);
+            pos++;
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Items); //어뎁터 생성
+
+        // 리스트뷰 생성 및 어뎁터 지정
+        list.setAdapter(adapter);
+
+        // 슬라이스 관련
+        slide = (SlideToActView)findViewById(R.id.slide);
+        slide.setOnSlideCompleteListener(new SlideToActView.OnSlideCompleteListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public  void onSlideComplete(SlideToActView slideToActView) {
+                // 세번밖에 안됨
+                finishAffinity();
+//                finish();
+            }
+        });
 
 
+    }
+
+    private void readWordData() {
+        InputStream inS = getResources().openRawResource(R.raw.word);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inS, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+        try {
+            // Step over headers
+            //reader.readLine();
+            int count = 0;
+            while ((line = reader.readLine()) != null) {
+
+                // Split by ','
+                String[] tokens = line.split(",");
+
+                // read the data
+                words[count] = tokens[0];
+                meaning[count] = tokens[1];
+
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
